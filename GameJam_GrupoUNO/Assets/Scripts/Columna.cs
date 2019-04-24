715f34c84a7768ef5c46f88e95f4719a03432b7b
Abel Zambrano Anacleto <37409439+Artego7@users.noreply.github.com>
2019-04-24 19:35:59 +0200

    Update

---
+++ GameJam_GrupoUNO/Assets/Scripts/Columna.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Columna : MonoBehaviour {
+
+
+    Rigidbody2D rbColum;
+    public float columVelocity;
+    //bool playerLive;
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rbColum.AddForce(new Vector2(columVelocity, 2), ForceMode2D.Impulse);
+            rbColum.gravityScale = 1.0f;
+        //if (playerLive) {
+        //}
+	}
+    
+}
---
+++ GameJam_GrupoUNO/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    GameObject player;
+    GameObject block;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        switch(collision.tag) {
+            case "Cuadrado":
+                break;
+            case "Triangulo":
+                break;
+            case "Circulo":
+                break;
+        }
+    }
+    
+       
+
+}
+
+
+
